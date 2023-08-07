5d4a82dca62110966540a4e2c36f606ceda8f999
hyunjjingjjing <141099571+hyunjjingjjing@users.noreply.github.com>
2023-08-07 22:51:26 +0900

    Add files via upload

---
+++ 찡과 친구되기.py
@@ -0,0 +1,65 @@
+print (' Welcome to Jjing Land')
+playing1=True
+if playing1:
+    choice1=input('찡은 야구시청을 좋아한다 o/x?:')
+    if choice1=="o":
+        print('딩동')
+    elif choice1=="x":
+        print('땡')
+        
+playing2=True
+if playing2:
+    choice2=input('찡은 요즘 운전을 하고 있다 o/x?:')
+    if choice2=="o":
+        print('딩동')
+    elif choice2=="x":
+        print('땡')
+        
+playing3=True
+if playing3:
+    choice3=input('찡은 해산물을 잘 먹는다 o/x?:')
+    if choice3=="o":
+        print('딩동')
+    elif choice3=="x":
+        print('땡')
+        
+playing4=True
+if playing4:
+    choice4=input('찡은 쭈꾸미에 대한 안좋은 기억이 있다 o/x?:')
+    if choice4=="o":
+        print('딩동')
+    elif choice4=="x":
+        print('땡. 어릴때 쭈꾸미 여러 마리가 쫓아오는 꿈을 꾸었음')
+        
+playing5=True
+if playing5:
+    choice5=input('찡은 길거리에서 비둘기를 봐도 아무렇지 않게 걸어간다 o/x?:')
+    if choice5=="o":
+        print('땡. 비둘기 극혐하고 무서워함')
+    elif choice5=="x":
+        print('딩동')
+        
+playing6=True
+if playing6:
+    choice6=input('찡의 오른손에는 점이 1개 있다 o/x?:')
+    if choice6=="o":
+        print('딩동')
+    elif choice6=="x":
+        print('땡')
+        
+playing7=True
+if playing7:
+    choice7=input('찡은 적절한 황금비율로 섞은 소맥을 좋아한다  o/x?:')
+    if choice7=="o":
+        print('땡. 찡은 소주파임')
+    elif choice7=="x":
+        print('딩동')
+        
+playing8=True
+if playing8:
+    choice8=int(input('딩동이 몇개 나왔나요?:'))
+    if choice8 > 4 :
+        print('찡과 친구되기 미션에 합격하신걸 축하드립니다^^')
+    elif choice7=="x":
+        print('할말이 없군요. 발전하세요.')
+print("Thankyou Bye!")
